13fc455756cf2546179c44cc97be09b196086401
amy140308 <58811108+amy140308@users.noreply.github.com>
2019-12-12 19:05:25 +0800

    Create news.py

---
+++ news.py
@@ -0,0 +1,56 @@
+import requests
+from bs4 import BeautifulSoup
+
+class news():
+    def __init__(self):
+        response = requests.get("https://nba.udn.com/nba/cate/6754/6780")
+        soup = BeautifulSoup(response.text, 'html.parser')
+        attr = {'id' : 'news_list_body'}
+        news_tag = soup.find_all('div', attrs = attr)
+        for i in news_tag:
+            self.pa = str(i)
+        self.data = list()
+
+    def get_news(self):
+        count = 0
+        hend = 0
+        tend = 0
+        tiend = 0
+        pend = 0
+        piend = 0
+        
+        while count < 3:
+            tmp = list()
+            hstart = self.pa.find('/nba/story/', hend)
+            hend = self.pa.find('">', hstart)
+            href = 'https://nba.udn.com' + self.pa[hstart:hend]
+
+            tstart = self.pa.find('<h3>', tend)
+            tend = self.pa.find('</h3>', tstart)
+            title = self.pa[tstart + 4:tend]
+
+            tistart = self.pa.find('h24">', tiend)
+            tiend = self.pa.find('</b>', tistart)
+            time = self.pa[tistart + 5:tiend]
+
+            pstart = self.pa.find('<p>', pend)
+            pend = self.pa.find('</p>', pstart)
+            p = self.pa[pstart + 3:pend]
+
+            pistart = self.pa.find('data-src', piend)
+            piend = self.pa.find('/><', pistart)
+            html = self.pa[pistart + 10:piend - 1]
+
+            count += 1
+            tmp.append(title)
+            tmp.append(time)
+            tmp.append(p)
+            tmp.append(href)
+            tmp.append(html)
+            self.data.append(tmp)
+
+        return self.data
+
+news = news()
+final = news.get_news()
+print(final)
